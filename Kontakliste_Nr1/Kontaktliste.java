cc490299c1b5cc79419a83c9fbf7e053197f85d1
G4lberSack <152169148+G4lberSack@users.noreply.github.com>
2023-11-27 12:13:39 +0000

    initial commit

---
+++ Kontakliste_Nr1/Kontaktliste.java
@@ -0,0 +1,7 @@
+public class Kontaktliste {
+
+    public static void main(String[] args) {
+        Kontakt k1 = new Kontakt("John", "Wick","017664293556");
+        System.out.println(k1.getName());
+    }
+}
