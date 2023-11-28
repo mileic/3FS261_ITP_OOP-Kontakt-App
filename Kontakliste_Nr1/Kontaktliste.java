fb00ab4569290d2d42ee2050ce4935f1810782e6
G4lberSack <152169148+G4lberSack@users.noreply.github.com>
2023-11-28 10:54:19 +0000

    update contactlist

--- Kontakliste_Nr1/Kontaktliste.java
+++
@@ -1,7 +0,0 @@
-public class Kontaktliste {
-
-    public static void main(String[] args) {
-        Kontakt k1 = new Kontakt("John", "Wick","017664293556");
-        System.out.println(k1.getName());
-    }
-}
