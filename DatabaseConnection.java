e16997c3bb8f3504c4e77d4312b1dcf621023f87
G4lberSack <152169148+G4lberSack@users.noreply.github.com>
2023-12-07 10:44:20 +0000

    connection sql

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -2,23 +2,29 @@ import java.sql.*;
 
 public class DatabaseConnection {
     public static void main(String[] args) {
+        // declare and initiate variables
         String url = "jbdc:mysql://localhost:3306/databasecontacts";
         String username = "root";
         String password = "Sportartikel-123"; 
 
         try {
+            // try to connect to database with JBDC driver. 
             Class.forName("com.mysql.cj.jbdc.Driver");
             Connection con = DriverManager.getConnection(url, username, password);
-            Statement statement = con.createStatement(); 
 
+            Statement statement = con.createStatement(); 
             ResultSet resultSet = statement.executeQuery("select * from contacts"); 
 
+            
             while(resultSet.next()) {
                 System.out.println(resultSet.getInt(1)+" "+resultSet.getString(2)+resultSet.getString(3)+resultSet.getString(4));
             }
 
+            // close connection to database 
             con.close();
+        
         } catch (Exception e) {
+            // connection not sucessful -> print error
             System.out.println(e);
         }
     } 
